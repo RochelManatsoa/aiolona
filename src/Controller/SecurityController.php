9b7f50b483bde0d8c41aec6abcb2bd6203b93493
rochel <rochel@rochel-ubuntu20.04>
2023-06-20 08:51:12 +0300

    knpu_oauth2 install

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    #[Route(path: '/login', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    #[Route(path: '/connect/google', name: 'connect_google_start')]
+    public function connectAction(ClientRegistry $clientRegistry)
+    {
+        return $clientRegistry
+            ->getClient('google_main') // key used in config/packages/knpu_oauth2_client.yaml
+            ->redirect();
+    }
+
+    #[Route(path: '/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    #[Route(path: '/connect/google/check', name: 'connect_google_check')]
+    public function connectCheckAction(Request $request, ClientRegistry $clientRegistry)
+    {
+        if(!$this->getUser()){
+            return new JsonResponse([
+                'status' => false,
+                'message' => "User not found"
+            ]);
+        }else{
+            return $this->redirectToRoute('app_dashboard');
+        }
+    }
+}
