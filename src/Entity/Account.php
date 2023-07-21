f6564e3eda3425325716252f0754f506c6e2359a
RochelManatsoa <nirinarochelmiandrisoa@gmail.com>
2023-07-21 13:08:57 +0300

    company registration + format code

--- src/Entity/Account.php
+++ src/Entity/Account.php
@@ -11,6 +11,9 @@ use Doctrine\ORM\Mapping as ORM;
 #[ORM\Entity(repositoryClass: AccountRepository::class)]
 class Account
 {
+    const RESSOURCE = 'ressource';
+    const EXPERT = 'expert';
+    
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
