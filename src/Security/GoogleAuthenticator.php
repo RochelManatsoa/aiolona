55923245890def4cdc390f5b91d2acbffa9ba25d
RochelManatsoa <nirinarochelmiandrisoa@gmail.com>
2023-09-05 14:49:28 +0300

    change expert profile, activate google auth registration mail

--- src/Security/GoogleAuthenticator.php
+++ src/Security/GoogleAuthenticator.php
@@ -1,39 +1,43 @@
 <?php
 namespace App\Security;
 
+use DateTime;
+use DateInterval;
 use App\Entity\User; 
 use App\Manager\IdentityManager;
+use App\Service\User\MailerService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\RouterInterface;
 use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use App\Service\Mailer\MailerService as MailerMailerService;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use KnpU\OAuth2ClientBundle\Security\Authenticator\OAuth2Authenticator;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Csrf\TokenGenerator\TokenGeneratorInterface;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
 
 class GoogleAuthenticator extends OAuth2Authenticator
 {
-    private $clientRegistry;
-    private $em;
-    private $router;
-    private $identityManager;
 
     public function __construct(
-        ClientRegistry $clientRegistry, 
-        EntityManagerInterface $em, 
-        RouterInterface $router,
-        IdentityManager $identityManager
+        private ClientRegistry $clientRegistry, 
+        private EntityManagerInterface $em, 
+        private RouterInterface $router,
+        private IdentityManager $identityManager,
+        private TokenGeneratorInterface $tokenGeneratorInterface,
+        private MailerMailerService $mailerService,
+        private RequestStack $requestStack,
     )
     {
-        $this->clientRegistry = $clientRegistry;
-        $this->em = $em;
-        $this->router = $router;
-        $this->identityManager = $identityManager;
+        
     }
 
     public function authenticate(Request $request): Passport
@@ -47,22 +51,44 @@ class GoogleAuthenticator extends OAuth2Authenticator
                 $googleUser = $client->fetchUserFromToken($accessToken);
 
                 $email = $googleUser->getEmail();
+                $tokenRegistration = $this->tokenGeneratorInterface->generateToken();
 
                 // have they logged in with Google before? Easy!
                 $existingUser = $this->em->getRepository(User::class)->findOneBy(['googleId' => $googleUser->getId()]);
 
                 //User doesnt exist, we create it !
+                $new = false;
                 if (!$existingUser) {
+                    $new = true;
                     $existingUser = new User();
                     $existingUser->setEmail($email);
                     $existingUser->setGoogleId($googleUser->getId());
+                    $existingUser->setTokenRegistration($tokenRegistration);
                     $existingUser->setIdentity($this->identityManager->init());
                 }
                 $existingUser->setFirstName($googleUser->getFirstName());
                 $existingUser->setLastName($googleUser->getLastName());
                 $existingUser->getIdentity()->setAvatar($googleUser->getAvatar());
+                $existingUser->setTokenRegistration($tokenRegistration);
                 $this->em->persist($existingUser);
                 $this->em->flush();
+                if($new){                    
+                    // send an email
+                    $this->mailerService->send(
+                        $email,
+                        "Confirmation du compte utilisateur",
+                        "registration_email.html.twig",
+                        [
+                            'user' => $existingUser,
+                            'token' => $tokenRegistration,
+                            'url' => $this->router->generate('account_verify', ['token' => $tokenRegistration, 'id' => $existingUser->getId()], UrlGeneratorInterface::ABSOLUTE_URL),
+                            'lifeTimeToken' => $existingUser->getTokenLifeTime()->format('d/M/Y à Hh:i')
+                        ]
+                    );
+                }
+
+    
+                $this->requestStack->getSession()->getFlashBag()->add('info', 'Votre compte a bien été crée, veuillez vérifier vos e-mails pour l\'activer.');
 
                 return $existingUser;
             })
@@ -74,9 +100,12 @@ class GoogleAuthenticator extends OAuth2Authenticator
         return $request->attributes->get('_route') === 'connect_google_check';
     }
 
-    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey) : Response
+    public function onAuthenticationSuccess(
+        Request $request, 
+        TokenInterface $token, 
+        $providerKey) : Response
     {
-        $targetUrl = $this->router->generate('app_account');
+        $targetUrl = $this->router->generate('app_login');
 
         return new RedirectResponse($targetUrl);
     }
